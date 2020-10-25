7c781a542b757a09090e626e2dff980c7b49e356
syedabuthahirm <abuthahir741@gmail.com>
2020-10-25 10:18:54 +0530

    Added React Support   - Added babel config for es6 module support   - Added React & ReactDOM   - Added babel-loader to transfer js|jsx file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Compiler Setup");
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Header from "./components/header.jsx";
+import Footer from "./components/footer";
+import Main from "./components/main";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Footer />
+      <Main />
+    </React.Fragment>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("react-app"));
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
         test: /\.html$/,
         use: [
           {
